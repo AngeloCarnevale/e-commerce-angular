9d72a89cbb8e0edffc0ba3ba5fce2d2e5e6c5b1f
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-05-03 15:29:38 -0300

    fix: auth ms

--- back-end/auth-ms/src/main/java/com/angelo/authms/controllers/AuthenticationController.java
+++ back-end/auth-ms/src/main/java/com/angelo/authms/controllers/AuthenticationController.java
@@ -7,6 +7,7 @@ import com.angelo.authms.domain.entities.user.User;
 import com.angelo.authms.domain.repositories.UserRepository;
 import com.angelo.authms.services.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -47,6 +48,6 @@ public class AuthenticationController {
 
         this.userRepository.save(newUser);
 
-        return ResponseEntity.ok().build();
+        return new ResponseEntity(newUser, HttpStatus.CREATED);
     }
 }
